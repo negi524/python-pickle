1c7a1554e9fe74df2e016935ceae05ad3ae80bd1
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-10-01 22:11:25 +0900

    feat: クラス作成

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+class User:
+    name: str
+    age: int
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+    def greet(self) -> None:
+        print("Hello.")
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_age(self) -> int:
+        return self.age
+
 
 def main():
-    print('hoge')
+    yamada = User("yamada", 20)
+    print(f"name={yamada.get_name()}, age={yamada.get_age()}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
